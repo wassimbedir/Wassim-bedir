7df3fbc503dadaea83574c5924b91432c4ceffef
wassimbedir <wassimbedir60@gmail.com>
2026-07-14 22:12:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -93,3 +93,27 @@ el.style.transition=".8s";
 observer.observe(el);
 
 });
+
+function searchPoems(){
+
+let input=document.getElementById("searchInput").value.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(function(card){
+
+let text=card.innerText.toLowerCase();
+
+if(text.includes(input)){
+
+card.style.display="block";
+
+}else{
+
+card.style.display="none";
+
+}
+
+});
+
+}
