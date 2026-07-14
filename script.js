efabfd34b6b4bf1b7b70ff1559d681def942a119
wassimbedir <wassimbedir60@gmail.com>
2026-07-14 22:16:53 +0100

    Remove searchPoems function
    Removed the searchPoems function from script.js.

--- script.js
+++ script.js
@@ -94,26 +94,3 @@ observer.observe(el);
 
 });
 
-function searchPoems(){
-
-let input=document.getElementById("searchInput").value.toLowerCase();
-
-let cards=document.querySelectorAll(".card");
-
-cards.forEach(function(card){
-
-let text=card.innerText.toLowerCase();
-
-if(text.includes(input)){
-
-card.style.display="block";
-
-}else{
-
-card.style.display="none";
-
-}
-
-});
-
-}
